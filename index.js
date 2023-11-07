55fb5f125532f3b47304671c507d506cf281fec9
Cristian Alaniz Benitez <alaniz21cris@gmail.com>
2023-11-06 22:01:07 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+const surprise = ["Guadalupe", "Ollie", "Aki"]
+
+const copyOfSurprise = []
+
+function writeCards(surprise) {
+    for (let i = 0; i < surprise.length; i++) {
+        copyOfSurprise.push(`Thank you, ${surprise[i]}, for the wonderful surprise gift!`);
+        debugger; 
+    }
+
+    return copyOfSurprise;
+}
+
+writeCards(copyOfSurprise);
+
+function countDown(startingNumber) {
+    let count = startingNumber;
+    while (count >= 0) {
+        console.log(count--);
+    } 
+
+}
+
+countDown(10);
